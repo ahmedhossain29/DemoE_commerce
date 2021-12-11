60a4ce31836a27e9c215cbd78c321a760491b8e5
unknown <you@example.com>
2021-12-12 00:57:28 +0600

    HOME Page

--- lib/ui/BottomNavPage/homeScreen.dart
+++ lib/ui/BottomNavPage/homeScreen.dart
@@ -1,3 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:e_commerce/const/AppColor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,63 +14,128 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  List<String> _carouselImage = [];
+  var _dotPosition = 0;
   TextEditingController _searchController = TextEditingController();
+
+  fatchCarouselImage() async {
+    var _firestoreInstance = FirebaseFirestore.instance;
+    QuerySnapshot qn =
+        await _firestoreInstance.collection("carousel-slider").get();
+    setState(() {
+      for (int i = 0; i < qn.docs.length; i++) {
+        _carouselImage.add(
+          qn.docs[i]["imgpath"],
+        );
+        print(qn.docs[i]["imgpath"]);
+      }
+    });
+
+    return qn.docs;
+  }
+
+  void initState() {
+    fatchCarouselImage();
+    super.initState();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 20.w, right: 20.w),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 60.h,
-                      child: TextFormField(
-                        controller: _searchController,
-                        decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          focusedBorder: OutlineInputBorder(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 60.h,
+                        child: TextFormField(
+                          controller: _searchController,
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(0)),
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                )),
+                            enabledBorder: OutlineInputBorder(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(0)),
                               borderSide: BorderSide(
-                                color: Colors.blue,
-                              )),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(0)),
-                            borderSide: BorderSide(
-                              color: Colors.grey,
+                                color: Colors.grey,
+                              ),
                             ),
+                            hintText: "Search Product Here",
+                            hintStyle: TextStyle(fontSize: 15.sp),
                           ),
-                          hintText: "Search Product Here",
-                          hintStyle: TextStyle(fontSize: 15.sp),
                         ),
                       ),
                     ),
-                  ),
-                  GestureDetector(
-                    child: Container(
-                      color: AppColors.deep_orange,
-                      height: 60.h,
-                      width: 60.h,
-                      child: Center(
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.white,
+                    GestureDetector(
+                      child: Container(
+                        color: AppColors.deep_orange,
+                        height: 60.h,
+                        width: 60.h,
+                        child: Center(
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
+                      onTap: () {},
                     ),
-                    onTap: () {},
-                  )
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 10.h,
+              ),
+              AspectRatio(
+                aspectRatio: 3.5,
+                child: CarouselSlider(
+                  items: _carouselImage
+                      .map((item) => Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(item),
+                                  fit: BoxFit.fitWidth))))
+                      .toList(),
+                  options: CarouselOptions(
+                      autoPlay: false,
+                      enlargeCenterPage: true,
+                      viewportFraction: 8.8,
+                      enlargeStrategy: CenterPageEnlargeStrategy.height,
+                      onPageChanged: (val, carouselPageChangedReason) {
+                        setState(() {
+                          _dotPosition = val;
+                        });
+                      }),
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              DotsIndicator(
+                dotsCount:
+                    _carouselImage.length == 0 ? 1 : _carouselImage.length,
+                position: _dotPosition.toDouble(),
+                decorator: DotsDecorator(
+                  activeColor: AppColors.deep_orange,
+                  spacing: EdgeInsets.all(2),
+                  activeSize: Size(8, 8),
+                  size: Size(6, 6),
+                  color: AppColors.deep_orange.withOpacity(0.5),
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
